c3e04e62b040feaccb3acdc206c2ff15a7fae318
GooRoo <sergey.olendarenko@gmail.com>
2022-04-08 02:27:42 +0200

    Testing units support (not bad so far)

---
+++ qml/easy/colors/units.js
@@ -0,0 +1,23 @@
+Object.defineProperty(Number.prototype, 'percent', {
+	get: function () {
+		return this / 100
+	}
+})
+
+Object.defineProperty(Number.prototype, '%', {
+	get: function () {
+		return this / 100
+	}
+})
+
+Object.defineProperty(Number.prototype, 'deg', {
+	get: function () {
+		return this / 360
+	}
+})
+
+Object.defineProperty(Number.prototype, '°', {
+	get: function () {
+		return this / 360
+	}
+})
